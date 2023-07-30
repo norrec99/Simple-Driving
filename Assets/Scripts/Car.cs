fdc341e8065223101a98365a12016b57c1aa215e
cagataysoyer <cagatay@stardust.gs>
2023-07-30 14:10:36 +0300

    add camera follow

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -6,11 +6,21 @@ public class Car : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     [SerializeField] private float speedIncreaseAmount = 0.1f;
+    [SerializeField] private float turnspeed = 200f;
+
+    private int steerValue;
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Rotate(0f, steerValue * turnspeed * Time.deltaTime, 0f);
+
         speed += speedIncreaseAmount * Time.deltaTime;
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
